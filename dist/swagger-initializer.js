2a8cb12580ea7b9f76ee6ef4465637e816d34b90
Jon-Mailes Graeffe <mail@jonni.it>
2023-06-16 06:52:47 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    urls: [
+      {url: 'https://raw.githubusercontent.com/netsys-lab/bittorrent-over-scion/feature/http-ui/http_api/openapi3.yaml', name: 'http-ui@bittorrent-over-scion'},
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
