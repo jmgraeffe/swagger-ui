1c412c84bc87da5dab3604f65d035a761b6db431
Jon-Mailes Graeffe <mail@jonni.it>
2024-10-07 19:55:27 +0200

    added selection of specification to display via query parameters

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,10 +1,31 @@
+const aliases = {
+  'bittorrent-over-scion': {
+    url: 'https://raw.githubusercontent.com/netsys-lab/bittorrent-over-scion/feature/http-ui/http_api/openapi3.yaml',
+    name: 'http-ui@bittorrent-over-scion'
+  },
+}
+
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
+
+  const params = new URLSearchParams(window.location.search);
+  const alias = params.get('alias');
+  const url = params.get('url');
+
+  var swaggerUrl = null;
+  if (alias != null && alias in aliases) {
+    swaggerUrl = aliases[alias];
+  } else if (url != null) {
+    swaggerUrl = {
+      url: url,
+      name: 'url'
+    };
+  }
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-      {url: 'https://raw.githubusercontent.com/netsys-lab/bittorrent-over-scion/feature/http-ui/http_api/openapi3.yaml', name: 'http-ui@bittorrent-over-scion'},
+      swaggerUrl,
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
